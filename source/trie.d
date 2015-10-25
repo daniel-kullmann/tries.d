5d16f1af507252aac24f85c43d53fdd0e1b031e1
Daniel Kullmann <daniel.kullmann@sap.com>
2015-10-25 06:35:58 +0100

    Simplifies code for add and check

--- source/trie.d
+++ source/trie.d
@@ -21,19 +21,11 @@ class Trie {
     }
 
     void add(string str) {
-        if (str == "") {
-            root.leaf = true;
-        } else {
-            add(&root, str, 0);
-        }
+        add(&root, str, 0);
     }
 
     bool check(string str) {
-        if (str == "") {
-            return root.leaf;
-        } else {
-            return check(root, str, 0);
-        }
+        return check(root, str, 0);
     }
 
     uint length() {
@@ -46,39 +38,37 @@ class Trie {
 
     private {
         void add(Item* item, string str, uint index) {
-            dchar character = str[index];
-            bool isLastCharacter = index == str.length-1;
-            foreach (ref child; item.children) {
-                if (child.character == character) {
-                    if (isLastCharacter) {
-                        child.leaf = true;
-                    } else {
+            bool isLastCharacter = index == str.length;
+            if (isLastCharacter) {
+                item.leaf = true;
+            } else {
+                dchar character = str[index];
+                foreach (ref child; item.children) {
+                    if (child.character == character) {
                         add(&child, str, index+1);
+                        return;
                     }
-                    return;
                 }
-            }
-            // no entry found; create new one
-            item.children.length += 1;
-            item.children[item.children.length-1] = Item(character, isLastCharacter);
-            if (!isLastCharacter) {
+                // no entry found; create new one
+                item.children.length += 1;
+                item.children[item.children.length-1] = Item(character, isLastCharacter);
                 add(&item.children[item.children.length-1], str, index+1);
             }
         }
 
         bool check(Item item, string str, uint index) {
-            dchar character = str[index];
-            bool isLastCharacter = index == str.length-1;
-            foreach (ref child; item.children) {
-                if (child.character == character) {
-                    if (isLastCharacter) {
-                        return child.leaf;
-                    } else {
+            bool isLastCharacter = index == str.length;
+            if (isLastCharacter) {
+                return item.leaf;
+            } else {
+                dchar character = str[index];
+                foreach (ref child; item.children) {
+                    if (child.character == character) {
                         return check(child, str, index+1);
                     }
                 }
+                return false;
             }
-            return false;
         }
 
         uint length(Item item) {
@@ -114,6 +104,9 @@ unittest {
     assert(!trie.check("a"));
     
     trie.add("abcd");
+    assert(trie.check("abcd"));
+    assert(!trie.check("a"));
+
     uint numberOfEntries = 0;
     bool correctEntry = true;
     trie.walker(delegate (string value) { 
@@ -124,9 +117,6 @@ unittest {
     });
     assert(numberOfEntries == 1);
     assert(correctEntry);
-
-    assert(trie.check("abcd"));
-    assert(!trie.check("a"));
 
     trie.add("a");
     assert(trie.check("a"));
