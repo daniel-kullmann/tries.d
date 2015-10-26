e449b4b249899a2c5aa670ca0b32f2406e60d61c
Daniel Kullmann <daniel.kullmann@sap.com>
2015-10-26 11:01:59 +0100

    Writes app that tests the trie.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,44 @@
 import std.stdio;
+import std.file;
+import std.string;
+import trie_data;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    auto trie = new DataTrie!string();
+
+    if (exists("/usr/share/dict/words")) {
+        File file = File("/usr/share/dict/words", "r");
+        string[] lines;
+        while (!file.eof()) {
+            string line = chomp(file.readln());
+            if (line != "") {
+                lines.length += 1;
+                lines[lines.length-1] = line;
+                trie.add(line, line);
+            }
+        }
+
+        foreach (line; lines) {
+            assert(trie.check(line));
+        }
+    }
+
+    if (exists("/usr/share/dict/cracklib-small")) {
+        File file = File("/usr/share/dict/cracklib-small", "r");
+        string[] lines;
+        while (!file.eof()) {
+            string line = chomp(file.readln());
+            if (line != "") {
+                lines.length += 1;
+                lines[lines.length-1] = line;
+                trie.add(line, line);
+            }
+        }
+
+        foreach (line; lines) {
+            assert(trie.check(line));
+        }
+    }
+
 }
