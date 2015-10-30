d2c40aaf9bc7c21f060ab8bc62bf097112d07e22
Daniel Kullmann <daniel.kullmann@sap.com>
2015-10-30 23:11:52 +0100

    example app compares trie performance against associative array. Result: the latter is much faster.

--- examples/wordlists/source/app.d
+++ examples/wordlists/source/app.d
@@ -6,6 +6,7 @@ import std.string;
 import trie_data;
 
 alias Trie = DataTrie!string;
+alias AA = string[string];
 
 void testWordFile(Trie trie, string filename, uint multiples) {
     if (exists(filename)) {
@@ -81,12 +82,100 @@ void testWordFile(Trie trie, string filename, uint multiples) {
     }
 }
 
+void testWordFile(AA aa, string filename, uint multiples) {
+    if (exists(filename)) {
+
+        StopWatch sw;
+        sw.reset();
+        sw.start();
+        File file = File(filename, "r");
+        string[] lines;
+        while (!file.eof()) {
+            string line = chomp(file.readln());
+            if (line != "") {
+                lines.length += 1;
+                lines[lines.length-1] = line;
+            }
+        }
+        sw.stop();
+        writeln("Read in " , lines.length, " lines in ", sw.peek().msecs, " ms");
+
+        sw.reset();
+        sw.start();
+        uint numItems = 0;
+        foreach (line; lines) {
+            auto addLine = line;
+            for (auto idx = 0; idx<multiples; idx++) {
+                aa[addLine] = line;
+                addLine ~= line;
+                numItems++;
+            }
+        }
+        sw.stop();
+        writeln("Added ", numItems, " items in ", sw.peek().msecs, " ms; ", 
+                1000 * to!double(sw.peek().msecs) / to!double(numItems), " us/item");
+
+        sw.reset();
+        sw.start();
+        foreach (line; lines) {
+            auto addLine = line;
+            for (auto idx = 0; idx<multiples; idx++) {
+                aa[addLine] = line;
+                addLine ~= line;
+            }
+        }
+        sw.stop();
+        writeln("Added ", numItems, " items again in ", sw.peek().msecs, " ms; ", 
+                1000 * to!double(sw.peek().msecs) / to!double(numItems), " us/item");
+
+        sw.reset();
+        sw.start();
+        foreach (line; lines) {
+            assert(line in aa);
+        }
+        sw.stop();
+        writeln("Checked ", aa.length, " items in ", sw.peek().msecs, " ms; ", 
+                1000 * to!double(sw.peek().msecs) / to!double(aa.length), " us/item");
+
+        uint count = 0;
+        foreach(name, ref value; aa) {
+            count += name.length;
+        }
+        writeln("AA stores strings of length " ~ to!string(count) ~ "");
+
+        foreach (line; lines) {
+           aa.remove(line);
+        }
+        sw.stop();
+        writeln("Removed ", lines.length, " items again in ", sw.peek().msecs, " ms; ", 
+                1000 * to!double(sw.peek().msecs) / to!double(lines.length), " us/item");
+
+        foreach (line; lines) {
+            assert(!(line in aa));
+        }
+    }
+}
+
 void main()
 {
+    string[string] aa;
+
+    writeln("# AA");
+    testWordFile(aa, "/usr/share/dict/cracklib-small", 10);
+    testWordFile(aa, "/usr/share/dict/american-english", 5);
+    testWordFile(aa, "/usr/share/dict/american-english-insane", 2);
+
+    aa = null;
+
+    writeln("");
+
     auto trie = new Trie();
 
+    writeln("# Trie");
     testWordFile(trie, "/usr/share/dict/cracklib-small", 10);
     testWordFile(trie, "/usr/share/dict/american-english", 5);
     testWordFile(trie, "/usr/share/dict/american-english-insane", 2);
+
+    trie = null;
 
 }
